5dd3ad0ea4119f252ac7734bbfbd7a911fc5c461
Ken Irving <fnkci@uaf.edu>
2011-03-21 08:33:11 -0800

    tob: moved tob script development directory from bin/.tob/ to tob/

---
+++ tob/build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+path=${0%/*}
+
+order=(shebang
+    prelude
+    warn
+    error
+    debug
+    parse-query
+    resolve-query
+    resolve-path
+    resolve-anonymous-object
+    resolve-named-object
+    resolve-library
+    resolve-attr-search-paths
+    resolve-search-paths
+    resolve-declaration
+    cull-duplicate-paths
+    resolve-method-path
+    resolve-chained-methods
+    run-chained-methods
+    TOB-resolve-method-path
+    TOB-get-attribute
+    main
+    )
+
+for f in ${order[*]}; do 
+    cat $path/$f
+    printf "\n"
+done
+
+printf "%s\n\n" 'main "$@"'
+
+cat $path/manpage
+
